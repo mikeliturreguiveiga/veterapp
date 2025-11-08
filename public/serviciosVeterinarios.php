cb1a47e7296ba4b45f1803d1d14494c476bb8007
Mikel <mikeliturreguiveiga@gmail.com>
2025-11-08 18:12:02 +0100

    comenzamos

---
+++ public/serviciosVeterinarios.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/serviciosVeterinarios.css">
+</head>
+<body>
+    <?php include("../includes/headerUsuario.php") ?>
+    <?php include("../includes/footerUsuario.php") ?>
+</body>
+</html>
