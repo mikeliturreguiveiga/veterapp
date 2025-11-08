047b169f21366b803ba5c5e078e285b1461d02d2
Mikel <mikeliturreguiveiga@gmail.com>
2025-11-08 18:05:16 +0100

    tienda-termiada

--- public/adopta.php
+++ public/adopta.php
@@ -26,84 +26,84 @@
                 <img src="../assets/img/animalesSlideShow/gato1.jpg" class="animalesGaleria" height="animales" alt="Animal 1">
                 <div class="infoAnimal">
                     <h3 class="nombreAnimal">Pelillos</h3>
-                    <h5 class="edadAnimal">2</h5>
+                    <h5 class="edadAnimal">2 años</h5>
                 </div>
             </div>
             <div class="tarjetaAdopta">
                 <img src="../assets/img/animalesSlideShow/gato2.jpg" class="animalesGaleria" height="animales" alt="Animal 2">
                 <div class="infoAnimal">
                     <h3 class="nombreAnimal">Mimines</h3>
-                    <h5 class="edadAnimal">4</h5>
+                    <h5 class="edadAnimal">4 años</h5>
                 </div>
             </div>
             <div class="tarjetaAdopta">
                 <img src="../assets/img/animalesSlideShow/gato3.jpg" class="animalesGaleria" height="animales" alt="Animal 3">
                 <div class="infoAnimal">
                     <h3 class="nombreAnimal">Esponjoso</h3>
-                    <h5 class="edadAnimal">2</h5>
+                    <h5 class="edadAnimal">2 años</h5>
                 </div>
             </div>
             <div class="tarjetaAdopta">
                 <img src="../assets/img/animalesSlideShow/perro1.jpg" class="animalesGaleria" height="animales" alt="Animal 4">
                 <div class="infoAnimal">
                     <h3 class="nombreAnimal">Thor</h3>
-                    <h5 class="edadAnimal">7</h5>
+                    <h5 class="edadAnimal">7 años</h5>
                 </div>
             </div>
             <div class="tarjetaAdopta">
                 <img src="../assets/img/animalesSlideShow/perro2.jpg" class="animalesGaleria" height="animales" alt="Animal 5">
                 <div class="infoAnimal">
                     <h3 class="nombreAnimal">Kenia</h3>
-                    <h5 class="edadAnimal">1</h5>
+                    <h5 class="edadAnimal">1 años</h5>
                 </div>
             </div>
             <div class="tarjetaAdopta">
                 <img src="../assets/img/animalesSlideShow/perro3.jpg" class="animalesGaleria" height="animales" alt="Animal 6">
                 <div class="infoAnimal">
                     <h3 class="nombreAnimal">Milka</h3>
-                    <h5 class="edadAnimal">2</h5>
+                    <h5 class="edadAnimal">2 años</h5>
                 </div>
             </div>
             <div class="tarjetaAdopta">
                 <img src="../assets/img/animalesSlideShow/perro4.jpg" class="animalesGaleria" height="animales" alt="Animal 7">
                 <div class="infoAnimal">
                     <h3 class="nombreAnimal">Beti</h3>
-                    <h5 class="edadAnimal">2</h5>
+                    <h5 class="edadAnimal">2 años</h5>
                 </div>
             </div>
             <div class="tarjetaAdopta">
                 <img src="../assets/img/animalesSlideShow/perro5.jpg" class="animalesGaleria" height="animales" alt="Animal 8">
                 <div class="infoAnimal">
                     <h3 class="nombreAnimal">Rayo</h3>
-                    <h5 class="edadAnimal">8</h5>
+                    <h5 class="edadAnimal">8 años</h5>
                 </div>
             </div>
             <div class="tarjetaAdopta">
                 <img src="../assets/img/animalesSlideShow/perro6.jpg" class="animalesGaleria" height="animales" alt="Animal 9">
                 <div class="infoAnimal">
                     <h3 class="nombreAnimal">Sultán</h3>
-                    <h5 class="edadAnimal">4</h5>
+                    <h5 class="edadAnimal">4 años</h5>
                 </div>
             </div>
             <div class="tarjetaAdopta">
                 <img src="../assets/img/animalesSlideShow/perro7.jpg" class="animalesGaleria" height="animales" alt="Animal 10">
                 <div class="infoAnimal">
                     <h3 class="nombreAnimal">Luna</h3>
-                    <h5 class="edadAnimal">11</h5>
+                    <h5 class="edadAnimal">11 años</h5>
                 </div>
             </div>
             <div class="tarjetaAdopta">
                 <img src="../assets/img/animalesSlideShow/perro8.jpg" class="animalesGaleria" height="animales" alt="Animal 11">
                 <div class="infoAnimal">
                     <h3 class="nombreAnimal">Chanel</h3>
-                    <h5 class="edadAnimal">2</h5>
+                    <h5 class="edadAnimal">2 años</h5>
                 </div>
             </div>
             <div class="tarjetaAdopta">
                 <img src="../assets/img/portadaAdopta.jpg" class="animalesGaleria" height="animales" alt="Animal 12">
                 <div class="infoAnimal">
                     <h3 class="nombreAnimal">Lobo</h3>
-                    <h5 class="edadAnimal">2</h5>
+                    <h5 class="edadAnimal">2 años</h5>
                 </div>
             </div>
         </div>
---
+++ public/tienda.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/tienda.css">
+</head>
+<body>
+    <?php include("../includes/headerUsuario.php") ?>
+    <div class="cabeceraTienda">
+        <div class="izquierda">
+            <h3>Alimentos, juguetes y más… porque ellos merecen lo mejor</h3>
+            <h5>Todos los productos deben ser adquiridos en nuestra tienda física</h5>
+        </div>
+        <div class="derecha">
+            <img src="../assets/img/tienda/comidaPortada.webp" alt="imagen portada">
+        </div>
+    </div>
+    <div class="contenedorAlimentos">
+        <div class="tarjeta">
+            <img src="../assets/img/tienda/comidaPerros.jpg" alt="">
+            <h2>Alimento para perros</h2>
+        </div>
+        <div class="tarjeta">
+            <img src="../assets/img/tienda/comidaAves.jpg.webp" alt="">
+            <h2>Alimento para aves</h2>
+        </div>
+        <div class="tarjeta">
+            <img src="../assets/img/tienda/comidaHurines.jpg" alt="">
+            <h2>Alimento para hurones</h2>
+        </div>
+        <div class="tarjeta">
+            <img src="../assets/img/tienda/comidaReptiles.jpg" alt="">
+            <h2>Alimento para reptiles</h2>
+        </div>
+        <div class="tarjeta">
+            <img src="../assets/img/tienda/comidaRoedores.jpg.png" alt="">
+            <h2>Alimento para roedores</h2>
+        </div>
+    </div>
+    <?php include("../includes/footerUsuario.php") ?>
+</body>
+</html>
