886c4d5faee468388f7b11b04f041bec5061ed10
Mikel <mikeliturreguiveiga@gmail.com>
2025-11-08 13:37:50 +0100

    termino-adopa-responsivo

--- public/adopta.php
+++ public/adopta.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>adopta</title>
     <link rel="stylesheet" href="../assets/css/adopta.css">
 </head>
+
 <body>
     <?php include("../includes/headerUsuario.php") ?>
     <div class="cabeceraAdopta">
@@ -15,11 +17,99 @@
             <h5>Para adoptar un animal llama a la tienda</h5>
         </div>
         <div class="derecha">
-            <img src="../assets/img/animalesSlideShow/perro4.jpg" alt="perro-cabecera">
+            <img src="../assets/img/portadaAdopta.jpg" alt="perro-cabecera">
         </div>
     </div>
-    <div class="galeriaImagenes">
+    <div class="divCajonGaleriaImagenes">
+        <div class="galeria">
+            <div class="tarjetaAdopta">
+                <img src="../assets/img/animalesSlideShow/gato1.jpg" class="animalesGaleria" height="animales" alt="Animal 1">
+                <div class="infoAnimal">
+                    <h3 class="nombreAnimal">Pelillos</h3>
+                    <h5 class="edadAnimal">2</h5>
+                </div>
+            </div>
+            <div class="tarjetaAdopta">
+                <img src="../assets/img/animalesSlideShow/gato2.jpg" class="animalesGaleria" height="animales" alt="Animal 2">
+                <div class="infoAnimal">
+                    <h3 class="nombreAnimal">Mimines</h3>
+                    <h5 class="edadAnimal">4</h5>
+                </div>
+            </div>
+            <div class="tarjetaAdopta">
+                <img src="../assets/img/animalesSlideShow/gato3.jpg" class="animalesGaleria" height="animales" alt="Animal 3">
+                <div class="infoAnimal">
+                    <h3 class="nombreAnimal">Esponjoso</h3>
+                    <h5 class="edadAnimal">2</h5>
+                </div>
+            </div>
+            <div class="tarjetaAdopta">
+                <img src="../assets/img/animalesSlideShow/perro1.jpg" class="animalesGaleria" height="animales" alt="Animal 4">
+                <div class="infoAnimal">
+                    <h3 class="nombreAnimal">Thor</h3>
+                    <h5 class="edadAnimal">7</h5>
+                </div>
+            </div>
+            <div class="tarjetaAdopta">
+                <img src="../assets/img/animalesSlideShow/perro2.jpg" class="animalesGaleria" height="animales" alt="Animal 5">
+                <div class="infoAnimal">
+                    <h3 class="nombreAnimal">Kenia</h3>
+                    <h5 class="edadAnimal">1</h5>
+                </div>
+            </div>
+            <div class="tarjetaAdopta">
+                <img src="../assets/img/animalesSlideShow/perro3.jpg" class="animalesGaleria" height="animales" alt="Animal 6">
+                <div class="infoAnimal">
+                    <h3 class="nombreAnimal">Milka</h3>
+                    <h5 class="edadAnimal">2</h5>
+                </div>
+            </div>
+            <div class="tarjetaAdopta">
+                <img src="../assets/img/animalesSlideShow/perro4.jpg" class="animalesGaleria" height="animales" alt="Animal 7">
+                <div class="infoAnimal">
+                    <h3 class="nombreAnimal">Beti</h3>
+                    <h5 class="edadAnimal">2</h5>
+                </div>
+            </div>
+            <div class="tarjetaAdopta">
+                <img src="../assets/img/animalesSlideShow/perro5.jpg" class="animalesGaleria" height="animales" alt="Animal 8">
+                <div class="infoAnimal">
+                    <h3 class="nombreAnimal">Rayo</h3>
+                    <h5 class="edadAnimal">8</h5>
+                </div>
+            </div>
+            <div class="tarjetaAdopta">
+                <img src="../assets/img/animalesSlideShow/perro6.jpg" class="animalesGaleria" height="animales" alt="Animal 9">
+                <div class="infoAnimal">
+                    <h3 class="nombreAnimal">Sultán</h3>
+                    <h5 class="edadAnimal">4</h5>
+                </div>
+            </div>
+            <div class="tarjetaAdopta">
+                <img src="../assets/img/animalesSlideShow/perro7.jpg" class="animalesGaleria" height="animales" alt="Animal 10">
+                <div class="infoAnimal">
+                    <h3 class="nombreAnimal">Luna</h3>
+                    <h5 class="edadAnimal">11</h5>
+                </div>
+            </div>
+            <div class="tarjetaAdopta">
+                <img src="../assets/img/animalesSlideShow/perro8.jpg" class="animalesGaleria" height="animales" alt="Animal 11">
+                <div class="infoAnimal">
+                    <h3 class="nombreAnimal">Chanel</h3>
+                    <h5 class="edadAnimal">2</h5>
+                </div>
+            </div>
+            <div class="tarjetaAdopta">
+                <img src="../assets/img/portadaAdopta.jpg" class="animalesGaleria" height="animales" alt="Animal 12">
+                <div class="infoAnimal">
+                    <h3 class="nombreAnimal">Lobo</h3>
+                    <h5 class="edadAnimal">2</h5>
+                </div>
+            </div>
+        </div>
+
     </div>
     <?php include("../includes/footerUsuario.php") ?>
 </body>
+
 </html>
