9afe3db331df0ef1163d0d57fe7ab0be015b47d3
Mikel <mikeliturreguiveiga@gmail.com>
2025-11-22 14:03:27 +0100

    esta-todo-menos-altura-de-tecnologia-equipo

---
+++ public/quienesSomos.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/quienesSomos.css">
+</head>
+
+<body>
+    <?php include("../includes/headerUsuario.php") ?>
+    <div class="contenedorImagen">
+        <img src="../assets/img/quienesSomos/portadaQuienesSomos.webp" alt="imagen portada quienes somos perro y gato">
+    </div>
+    <div class="contenedorGeneralQuienesSomos">
+        <p>En Torrevieja, somos mucho más que una clínica;
+            somos el hogar de la salud y el bienestar de tus mascotas en Torrevieja.
+            Nacimos de una profunda vocación: ofrecer el cuidado más profesional y,
+            sobre todo, el más cariñoso a cada animal que cruza nuestras puertas.
+        </p>
+        <h1>
+            Valoración y cariño
+        </h1>
+        <p>
+            Sabemos que tu mascota es un miembro más de la familia. Por ello, el amor,
+            el respeto y la dedicación son los pilares de nuestro trabajo diario. Tratamos
+            a cada perro, gato, y animal exótico con la misma ternura y atención que pondrías
+            tú mismo. Nuestro compromiso es garantizar que se sientan seguros, cómodos y
+            queridos en todo momento.
+        </p>
+        <img src="../assets/img/quienesSomos/imagenContenedor.jpg" alt="imagen contenedor quienes somos">
+        <div class="contenedorTecnologiaDedicados">
+            <div class="contenedorTecnologia">
+                <h1>Tecnología y Diagnóstico de Vanguardia</h1>
+                <p>Para complementar nuestro trato humano, contamos con instalaciones y
+                    equipamiento clínico de última generación. Ponemos a disposición de tu
+                    mascota todo el material necesario para realizar diagnósticos precisos y
+                    tratamientos eficaces. Desde pruebas de laboratorio completas y equipamiento
+                    de imagen avanzado (radiología digital, ecografía), hasta quirófanos
+                    totalmente equipados, garantizamos que tu animal recibirá la mejor atención
+                    médica posible.
+                </p>
+            </div>
+            <div class="contenedorEqupo">
+                <h1>Un Equipo de Profesionales Dedicados</h1>
+                <p>Detrás de cada diagnóstico y cada tratamiento hay un gran equipo de
+                    profesionales altamente cualificados. Nuestra plantilla está formada por
+                    veterinarios con diferentes especialidades y auxiliares técnicos de
+                    veterinaria (ATVs) experimentados, todos unidos por una pasión común: la
+                    salud animal. Estamos en continua formación para aplicar las técnicas más
+                    innovadoras y las mejores prácticas en medicina veterinaria.
+                </p>
+            </div>
+
+        </div>
+    </div>
+    <?php include("../includes/footerUsuario.php") ?>
+</body>
+
+</html>
