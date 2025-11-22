d808c74b6ecf91a66933fe69a424beff5fe21a0c
Mikel <mikeliturreguiveiga@gmail.com>
2025-11-22 18:11:46 +0100

    avanzamos-en-el-aspecto-del-login

---
+++ public/areaPersonal.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/areaPersonal.css">
+</head>
+
+<body>
+    <div class="contenedorAreaPersonal">
+        <img class="perroPortadaAreaPersonal" src="../assets/img/areaPersonal/perroAreaPersonal.jpeg" alt="perro portada area personal">
+        <div class="contenedorLogin">
+            <div class="contenedorLogoClinica">
+                <a href="../public/index.php"><img src="../assets/img/logoClinica.svg" alt="logoClinica" class="logoClinica" name="logoClinica"></a>
+            </div>
+            <form action="" method="post">
+                <div class="divNombre">
+                    <img src="../assets/img/areaPersonal/nombre.svg" alt="">
+                    <input type="text" name="nombre" id="nombre" placeholder="Nombre">
+                </div>
+                <div class="divContraseña">
+                    <img src="../assets/img/areaPersonal/contraseña.svg" alt="">
+                    <input type="password" name="contraseña" id="contraseña" placeholder="Contraseña">
+                </div>
+                <input class="botonInicioSesion" type="submit" value="Iniciar">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
